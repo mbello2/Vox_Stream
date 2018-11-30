d608450793e228c3a9e11e33b3ebd067c03d2aac
mbello2 <maxtv@hotmail.com.br>
2018-11-30 11:18:55 -0200

    Update updatenew.sh

--- updatenew.sh
+++ updatenew.sh
@@ -15,7 +15,7 @@ id | grep "uid=0(" >/dev/null
 if [ $? != "0" ]; then
 	uname -a | grep -i CYGWIN >/dev/null
 	if [ $? != "0" ]; then
-		echo "Erro ao instalar o voxtream, você não esta logado como usuário root.";
+		echo "Erro ao instalar o voxtream, vocÃª nÃ£o esta logado como usuÃ¡rio root.";
 		echo "";
                         rm -f installr*;
 		exit 1;
@@ -27,7 +27,7 @@ os=`uname`
 licensa=$1
 if  [ $os = "FreeBSD" ]; then
 echo ""
-echo "Instalação criada pela SCRIPT CERTO."
+echo "InstalaÃ§Ã£o criada pela SCRIPT CERTO."
 echo ""
 exit 1;
 fi
@@ -38,19 +38,19 @@ chmod 777 /home/streaming;
 cd /etc/default/;
 mkdir hora;
 cd /etc/default/hora/;
-wget http://picstream.ml/painel_voxstream/hora.zip;unzip hora.zip;rm hora.zip;
+wget https://github.com/mbello2/Vox_Stream/raw/master/hora.zip;unzip hora.zip;rm hora.zip;
 cd /home/streaming/;
 mkdir horario;
-rm horah;wget http://picstream.ml/painel_voxstream/cron.zip;unzip cron.zip;rm cron.zip;chmod +x /home/streaming/cron;
-wget http://picstream.ml/painel_voxstream/shoutcast.zip;unzip shoutcast.zip;rm shoutcast.zip;
+rm horah;wget https://github.com/mbello2/Vox_Stream/raw/master/cron.zip;unzip cron.zip;rm cron.zip;chmod +x /home/streaming/cron;
+wget https://github.com/mbello2/Vox_Stream/raw/master/shoutcast.zip;unzip shoutcast.zip;rm shoutcast.zip;
 chmod +x shoutcast/v1/ligar_autodj;
 chmod +x shoutcast/v1/ligar_streaming;
 chmod +x shoutcast/v2/ligar_autodj;
 chmod +x shoutcast/v2/ligar_streaming;
 find /shoutcast -type d -exec chmod 777 {} \;
 cd /home/streaming/playlists;
-wget http://picstream.ml/painel_voxstream/Feminina.pls;
-wget http://picstream.ml/painel_voxstream/Masculina.pls;
+wget https://github.com/mbello2/Vox_Stream/blob/master/Feminina.pls;
+wget https://github.com/mbello2/Vox_Stream/blob/master/Masculina.pls;
 
 clear;
 echo ""
